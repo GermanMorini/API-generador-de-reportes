ed999235ce77d3187e317b16120ef7e000e229c9
Germán Morini <germorini25@gmail.com>
2023-12-16 20:15:41 -0300

    * Añadido otro reporte mas completo * "doc1" y "doc2" para obtener los reportes

--- src/main/java/com/report/Controller.java
+++ src/main/java/com/report/Controller.java
@@ -1,7 +1,6 @@
 package com.report;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.sf.jasperreports.engine.*;
 import org.springframework.http.HttpStatus;
@@ -12,30 +11,32 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/api/docs")
+@RequestMapping("/api/")
 @Slf4j
 public class Controller {
 
-      private final String templatePath = "src/main/resources/templates/report1.jrxml";
+      private final String templatePath = "src/main/resources/templates/";
 
-      @PostMapping
-      public ResponseEntity<byte[]> genDocument(@RequestBody Parameters params) throws JRException {
+      @PostMapping("doc1")
+      public ResponseEntity<byte[]> genRepo1(@RequestBody Reporte1 params) throws JRException {
             LocalDateTime fecha = LocalDateTime.now();
             DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
 
             HashMap<String, Object> p = new HashMap<>();
-            p.put("comprobante_id", params.getComprobante_id());
-            p.put("fecha", df.format(fecha));
-            p.put("total", params.getTotal());
+
             // classpath es el directorio resources
             p.put("imageDir", "classpath:/static/");
+            p.put("comprobante_id", params.comprobante_id);
+            p.put("fecha", df.format(fecha));
+            p.put("total", params.total);
 
-            JasperReport repo = JasperCompileManager.compileReport(templatePath);
+            JasperReport repo = JasperCompileManager.compileReport(templatePath + "report1.jrxml");
             JasperPrint jp = JasperFillManager.fillReport(repo, p, new JREmptyDataSource());
 
             // ResponseEntity es una clase que permite dar una respuesta personalizada
@@ -45,8 +46,43 @@ public class Controller {
                     .body(JasperExportManager.exportReportToPdf(jp)); // el cuerpo de la respuesta
       }
 
-      @Getter @Setter
-      static private class Parameters {
+      @PostMapping("doc2")
+      public ResponseEntity<byte[]> genRepo2(@RequestBody Reporte2 params) throws JRException {
+            LocalDate ldt = LocalDate.now();
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yy");
+
+            HashMap<String, Object> p = new HashMap<>();
+
+            p.put("imageDir", "classpath:/static/");
+            p.put("dni", params.dni);
+            p.put("nombre_estudiante", params.nombre_estudiante);
+            p.put("clave", params.clave);
+            p.put("legajo", params.legajo);
+            p.put("carrera", params.carrera);
+            p.put("plan_estudios", params.plan_estudios);
+            p.put("fecha_examen", params.fecha_examen);
+            p.put("hora_apertura", params.hora_apertura);
+            p.put("dia_emision", dtf.format(ldt));
+            p.put("validacion", params.validacion);
+            p.put("validez", dtf.format(ldt.plusMonths(3).minusDays(10)));
+
+            JasperReport repo = JasperCompileManager.compileReport(templatePath + "report2.jrxml");
+            JasperPrint jp = JasperFillManager.fillReport(repo, p, new JREmptyDataSource());
+
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .body(JasperExportManager.exportReportToPdf(jp));
+      }
+
+      // el getter hace falta, porque sino no puede leer la info que le pasamos
+      @Getter
+      static private class Reporte1 {
             private String comprobante_id, total;
+      }
+
+      @Getter
+      static private class Reporte2 {
+            private String dni, nombre_estudiante, clave, legajo, carrera, plan_estudios, fecha_examen, hora_apertura, validacion;
       }
 }
